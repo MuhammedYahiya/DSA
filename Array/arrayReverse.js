a07307d944798d205717cb05fecd42b5c3ea2977
MuhammedYahiya <yahiyahmhd@gmail.com>
2024-07-18 08:17:26 +0530

    reverse array

--- Array/arrayReverse.js
+++ Array/arrayReverse.js
@@ -1,4 +1,6 @@
 const arr = [1, 2, 3, 4, 5, 6];
+const start = 0
+const end = arr.length-1
 
 let normalReverse = (arr) => {
   for (let i = arr.length - 1; i >= 0; i--) {
@@ -6,8 +8,25 @@ let normalReverse = (arr) => {
   }
 };
 
+let usingWhileLoop = (arr,start,end) =>{
+  while(start < end){
+    let temp = arr[start]
+    arr[start] = arr[end]
+    arr[end] = temp
+    end--
+    start++
+  }
+}
+
+let printArray = (arr) =>{
+  for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+  }
+}
+
 //Array Reverse Using inbuilt function
 let arrayReverseUsingInbuiltFunc = (arr, len) => {
   let reverseArray = arr.slice().reverse();
   return reverseArray;
 };
+
